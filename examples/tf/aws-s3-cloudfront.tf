3b1f5b32dd804fef0b7a86d784a9b143921f4b93
robertlestak <robert.lestak@umusic.com>
2024-03-10 16:02:17 -0700

    tf output cf distribution id

--- examples/tf/aws-s3-cloudfront.tf
+++ examples/tf/aws-s3-cloudfront.tf
@@ -96,3 +96,7 @@ resource "aws_cloudfront_distribution" "my_distribution" {
 output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.my_distribution.domain_name
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.my_distribution.id
+}
